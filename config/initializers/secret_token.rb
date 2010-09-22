1af10e5801313904a130ca9c1cbf95eb09e473ea
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-09-22 18:21:27 +0200

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightningtalks::Application.config.secret_token = '9d701d7d6a8a0a98ae8a4c8fa1e46801799f8e1362bfd76f0153c0af4b1c54151d3c4d61d55c668115f8428fe9ac1673efc502188ce353528103a58322603f44'
